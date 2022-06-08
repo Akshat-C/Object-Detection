191d1cf64cee4d16b38af81f30762f51e631923b
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-06-08 19:12:06 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status1 = "";
+
 function preload()
 {
     img = loadImage("dog_cat.jpg");
@@ -7,6 +9,27 @@ function setup()
 {
     canvas = createCanvas(550, 500);
     canvas.center();
+
+    o_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Objects Detecting";
+}
+
+function modelLoaded()
+{
+    console.log("Object Detector has been loaded");
+    status1 = true;
+    o_detector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.error()
+    } else 
+    {
+        console.log(results);
+    }
 }
 
 function draw()
@@ -17,4 +40,10 @@ function draw()
     noFill();
     stroke("red");
     rect(50, 70, 400, 300);
+
+    fill("red");
+    text("cat", 260, 130);
+    noFill();
+    stroke("red");
+    rect(250, 110, 400, 300);
 }
